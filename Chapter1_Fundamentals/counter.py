fa2b795e57c94eb72698a309f5e724ae3eddf24f
Lucas Cheng <chengyoufu@gmail.com>
2018-07-17 22:02:22 +0800

    update Counter

--- Chapter1_Fundamentals/counter.py
+++ Chapter1_Fundamentals/counter.py
@@ -10,13 +10,14 @@ import threading
 
 class Counter(object):
 
-    def __init__(self, counter=0, max_value=10000):
-        self.__counter = counter
+    def __init__(self, counter_id, max_value=10000):
+        self.__counter_id = counter_id
+        self.__counter = 0
         self.__lock = threading.Lock()
         self.__max_value = max_value
 
-    def reset_counter(self, counter=0):
-        self.__counter = counter
+    def reset_counter(self):
+        self.__counter = 0
 
     @property
     def counter(self):
@@ -26,7 +27,12 @@ class Counter(object):
         with self.__lock:
             self.__counter += 1
         if self.__counter > self.__max_value:
-            raise ValueError("counter {0} is exceed limit {1}".format(self.counter,self.__max_value))
+            raise ValueError("counter {0} is exceed limit {1}".format(self.counter, self.__max_value))
+
+    def __str__(self):
+        return "Counter ID:{0}-->counter number is {1}".format(self.__counter_id, self.counter)
+
+    __repr__ = __str__
 
 
 if __name__ == '__main__':
