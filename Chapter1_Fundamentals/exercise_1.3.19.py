67683cf05c5c15da0bd8292089fa5bce02866228
Lucas Cheng <chengyoufu@gmail.com>
2018-07-26 23:04:44 +0800

    add 1.3.21

---
+++ Chapter1_Fundamentals/exercise_1.3.19.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/26 22:53
+# @Author  : leo cheng
+# @Email   : chengyoufu@163.com
+# @File    : exercise_1.3.19.py
+# @Software: PyCharm
+
+
+def find_list(list,key):
+    for item in list:
+        if item.item_value == key:
+            return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    pass
+    
+    
