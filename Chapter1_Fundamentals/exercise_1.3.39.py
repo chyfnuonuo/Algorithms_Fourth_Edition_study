c2a14795a88c599440d37c1243a58d4c61f0501f
Lucas Cheng <chengyoufu@gmail.com>
2018-07-31 22:24:46 +0800

    add exercise1.3.39

---
+++ Chapter1_Fundamentals/exercise_1.3.39.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/31 22:17
+# @Author  : leo cheng
+# @Email   : chengyoufu@163.com
+# @File    : exercise_1.3.39.py
+# @Software: PyCharm
+
+
+class RingBuffer(object):
+
+    def __init__(self):
+        pass
+
+    def is_empty(self):
+        return True
+
+    def enqueue(self,item):
+        pass
+
+    def dequeue(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    pass
+    
+    
