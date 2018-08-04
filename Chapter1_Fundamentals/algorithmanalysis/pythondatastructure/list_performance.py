3a28457b303ab757a11320fa68db30efc846c88e
Lucas Cheng <chengyoufu@gmail.com>
2018-08-04 22:09:57 +0800

    add list analysis

--- Chapter1_Fundamentals/algorithmanalysis/pythondatastructure/list_performance.py
+++ Chapter1_Fundamentals/algorithmanalysis/pythondatastructure/list_performance.py
@@ -6,7 +6,7 @@
 # @File    : list_performance.py
 # @Software: PyCharm
 from timeit import Timer
-
+import matplotlib.pyplot as plt
 
 def test1():
     list_data = []
@@ -28,6 +28,14 @@ def test4():
     list_data = list(range(1000))
 
 
+def show_fig(self):
+    x = range(self.__data_num)
+    plt.scatter(x, self.data_list)
+    z = [sum(self.data_list) / self.__data_num] * self.__data_num
+    plt.plot(x, z, label="$mean value$", color="red", linewidth=2)
+    plt.show()
+
+
 if __name__ == '__main__':
     t1 = Timer('test1()', 'from __main__ import test1')
     t2 = Timer('test2()', 'from __main__ import test2')
@@ -37,3 +45,18 @@ if __name__ == '__main__':
     print('append', t2.timeit(number=1000), 'ms')
     print('comprehension', t3.timeit(number=1000), 'ms')
     print('list range', t4.timeit(number=1000), 'ms')
+
+    list_data = list(range(10000000))
+    pop_zero = Timer('list_data.pop(0)', 'from __main__ import list_data')
+    pop_end = Timer('list_data.pop()', 'from __main__ import list_data')
+    print('pop zero',pop_zero.timeit(number=1000),'ms')
+    print('pop', pop_end.timeit(number=1000), 'ms')
+
+    pop_zero = Timer('list_data.pop(0)', 'from __main__ import list_data')
+    pop_end = Timer('list_data.pop()', 'from __main__ import list_data')
+    for i in range(1000000, 100000001, 1000000):
+        list_data = list(range(i))
+        pt = pop_end.timeit(number=1000)
+        list_data = list(range(i))
+        pz = pop_zero.timeit(number=1000)
+        print("%15.5f,	%15.5f" % (pz, pt))
