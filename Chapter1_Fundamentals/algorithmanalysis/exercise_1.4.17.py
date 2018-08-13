243041d283c810372585fd9bde1bb6d48bd54533
Lucas Cheng <chengyoufu@gmail.com>
2018-08-13 22:57:47 +0800

    add exercise 1.4.17

---
+++ Chapter1_Fundamentals/algorithmanalysis/exercise_1.4.17.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/13 22:52
+# @Author  : leo cheng
+# @Email   : chengyoufu@163.com
+# @File    : exercise_1.4.17.py
+# @Software: PyCharm
+
+
+def faster_pair(data_list):
+    min = data_list[0]
+    max = data_list[0]
+    min_index = 0
+    max_index = 0
+    for index, data in enumerate(data_list):
+        if data < min:
+            min = data
+            min_index = index
+        elif data > max:
+            max = data
+            max_index = index
+
+    return min_index, max_index
+
+
+if __name__ == '__main__':
+    print(faster_pair([0.1, -2.3, 4.5, 1.2, 6.1, -1.1]))
