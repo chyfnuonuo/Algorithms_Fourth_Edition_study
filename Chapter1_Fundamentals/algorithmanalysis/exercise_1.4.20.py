fa3d7e9f71913b761b6619cc555729f9d33d8283
Lucas Cheng <chengyoufu@gmail.com>
2018-08-18 22:13:12 +0800

    fix bugs

--- Chapter1_Fundamentals/algorithmanalysis/exercise_1.4.20.py
+++ Chapter1_Fundamentals/algorithmanalysis/exercise_1.4.20.py
@@ -12,38 +12,43 @@ def find_max(data_list):
     begin = 0
     end = len(data_list) - 1
     result_index = -1
-    while begin <= end:
+    while begin < end:
         mid = (begin + end) // 2
         if data_list[mid] < data_list[mid + 1]:
-            begin = mid
+            begin = mid+1
         elif data_list[mid] < data_list[mid - 1]:
-            end = mid
+            end = mid-1
         else:
             result_index = mid
             break
+    if begin == end:
+        result_index = begin
     return result_index
 
 
-def doubly_search(data_list, search_item):
-    max_index = find_max(data_list)
-    if max_index == -1:
+def doubly_search(data, item):
+    max = find_max(data)
+    if max == -1:
         raise ValueError
-    result_list = []
-    if search_item > data_list[max_index] or (search_item < data_list[0] and search_item < data_list[-1]):
+    result = []
+    if item > data[max] or (item < data[0] and item < data[-1]):
         return -1
+    elif item == data[max]:
+        result.append(max)
+    else:
+        if data[0] < item < data[max]:
+            low = binary_search(data[:max], item)
+            if low != -1:
+                result.append(low)
+        if data[-1] < item <= data[max]:
+            high = binary_search(data[max:], item, reverse=True)
+            if high != -1:
+                result.append(high + max)
 
-    if data_list[0] < search_item < data_list[max_index]:
-        low_result = binary_search(data_list[:max_index], search_item)
-        if low_result != -1:
-            result_list.append(low_result)
-    if data_list[-1] < search_item <= data_list[max_index]:
-        high_result = binary_search(data_list[max_index:], search_item, reverse=True)
-        if high_result != -1:
-            result_list.append(high_result + max_index)
-    if len(result_list) == 0:
-        result_list.append(-1)
+    if len(result) == 0:
+        result.append(-1)
 
-    return tuple(result_list)
+    return tuple(result)
 
 
 if __name__ == '__main__':
@@ -51,3 +56,4 @@ if __name__ == '__main__':
     print(doubly_search([1, 3, 4, 6, 7, 8, 9, 6, 3, 2, 1, 0], 3))
     print(doubly_search([1, 3, 4, 6, 7, 8, 9, 6, 3, 2, 1, 0], 9))
     print(doubly_search([1, 3, 4, 6, 7, 8, 9], 9))
+
